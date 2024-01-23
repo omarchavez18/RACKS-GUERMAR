54dd74363d6749645ce5bc0adc4b05c1206de416
Omar <omarchavezgutierrezdevelasco@gmail.com>
2024-01-23 16:14:35 -0600

    keywords section deleted

--- src/app/productos/page.jsx
+++ src/app/productos/page.jsx
@@ -3,9 +3,6 @@ export const metadata = {
   title: 'Racks Guermar | Productos',
   description:
     ' Ofrecemos una amplia gama de productos como rack selectivo de carga pesada, rack picking, anaqueles, mezzanine, estanterias con y sin tornillos, mesas metalicas, escaleras industriales, elevadores, cajon homero y pico de pato, y gabinetes.',
-  keywords: [
-    'racks, rack picking, mezzanine, anaquel, estanteria, escaleras, mesas, elevadores, cajon hormero, gabinete',
-  ],
 }
 
 const Productos = () => {
--- src/app/quienes-somos/page.jsx
+++ src/app/quienes-somos/page.jsx
@@ -4,7 +4,6 @@ export const metadata = {
   title: 'Racks Guermar | Quienes Somos',
   description:
     'En Guermar empresa 100% mexicana, somos fabricantes con experiencia de más de 30 años en soluciones de almacenamiento. Ofrecemos una amplia gama de racks y estanterías metálicas diseñadas para maximizar la eficiencia y el espacio en tu negocio.',
-  keywords: ['racks guermar, guermar '],
 }
 
 function QuienesSomos() {
