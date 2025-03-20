a42ab2588a23b6ea39f228338fe316cd2f0f1206
Omar <omarchavezgutierrezdevelasco@gmail.com>
2025-03-19 19:21:28 -0600

    google tag manager added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,7 @@ import BootstrapClient from '@/components/bootstrapClient/BootstrapClient'
 import Footer from '@/components/footer/Footer'
 import FloatingButton from '@/components/floatingButton/FloatingButton'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata = {
   title: 'Racks Guermar | Inicio',
@@ -30,6 +31,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <GoogleTagManager gtmId='GTM-M3Z3J7RR' />
+      </head>
       <body className={openSans.className}>
         <Navbar />
         {children}
